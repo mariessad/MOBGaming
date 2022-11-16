cba1d81a247db34f8de42c99bc20c2b9d5e9dee0
mariessad <74148632+mariessad@users.noreply.github.com>
2022-11-16 02:09:28 -0500

    changes to package.json, changes to server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -28,6 +28,15 @@ app.use(express.json());
 
 app.use(cors());
 // routes
+
+// production code
+if (process.env.NODE_ENV === 'production') {
+  //*Set static folder up in production
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => res.sendFile(path.resolve(__dirname, 'client', 'build','index.html')));
+}
+  //
 app.use("/api/users", userRouter);
 
 app.all("*", (request, response) => {
